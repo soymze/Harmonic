b2cd5476fb9611d9b2f7afd7510a7b93dae0d36d
soymze <soylmze@gmail.com>
2024-01-23 19:53:26 +0300

    asdad

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int sayi;
+        double result=0.0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Sayıyı giriniz :");
+        sayi=sc.nextInt();
+
+        for(double i=1; i<=sayi; i++){
+            result += (1/i);
+        }
+
+        System.out.println("Sonuç : " +result);
     }
 }
